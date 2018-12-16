d8be4ee2e02008de43779f1c6e285973cd89121d
MagdziaKuba <42269949+MagdziaKuba@users.noreply.github.com>
2018-12-16 22:04:05 +0100

    makeover

--- src/hamburger.js
+++ src/hamburger.js
@@ -5,24 +5,24 @@ hamburger.addEventListener("click", function(){
 },false);
 
 function mouseOver(arg){
-      var arg=arg;
-      var square=document.getElementsByClassName("square");
-        square[arg].style.border="1px solid #e84c3c";
-        square[arg].style.transform="rotate(-45deg)";
-        var images=document.getElementsByClassName("images");
-        images[arg].style.transform="rotate(45deg)";
-        var h3Text=document.getElementsByClassName("h3Text");
-        h3Text[arg].style.color="#e84c3c";
-    }
-    function mouseOut(arg){
-      var arg=arg;
-      var square=document.getElementsByClassName("square");
-        square[arg].style.border="1px solid #CCCCCC";
-        square[arg].style.transform="rotate(0deg)";
-        var images=document.getElementsByClassName("images");
-        images[arg].style.transform="rotate(0deg)";
-        var h3Text=document.getElementsByClassName("h3Text");
-        h3Text[arg].style.color="#333333";
-    }
+    var arg=arg;
+    var square=document.getElementsByClassName("square");
+    square[arg].style.border="1px solid #e84c3c";
+    square[arg].style.transform="rotate(-45deg)";
+    var images=document.getElementsByClassName("images");
+    images[arg].style.transform="rotate(45deg)";
+    var h3Text=document.getElementsByClassName("h3Text");
+    h3Text[arg].style.color="#e84c3c";
+}
+function mouseOut(arg){
+    var arg=arg;
+    var square=document.getElementsByClassName("square");
+    square[arg].style.border="1px solid #CCCCCC";
+    square[arg].style.transform="rotate(0deg)";
+    var images=document.getElementsByClassName("images");
+    images[arg].style.transform="rotate(0deg)";
+    var h3Text=document.getElementsByClassName("h3Text");
+    h3Text[arg].style.color="#333333";
+}
 
 
